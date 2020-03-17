6ce945d2d94bc58da0ec0482ee5dcbb98af19f95
olegzhuk <oleg.zhuk@odysseusinc.com>
2020-03-17 18:42:14 +0300

    Acute respiratory insufficiency added

---
+++ Vocabulary/sql/phenotypes/Acute respiratory insufficiency.sql
@@ -0,0 +1,399 @@
+--reset phenotype concept list
+DELETE FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+;
+
+--reset Standard concepts Included list
+DELETE FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'inclusion'
+;
+
+--List of Standard concepts Included
+INSERT INTO dev_covid19.concept_phenotypes
+SELECT 'Acute respiratory insufficiency', 'inclusion', c.*
+FROM devv5.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+319049  	--	65710008	Condition	Acute respiratory failure	SNOMED
+
+    )
+;
+
+--List of Standard concepts Included for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'inclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Included
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'inclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM devv5.concept_ancestor ca1
+JOIN devv5.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN devv5.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN devv5.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM dev_covid19.concept_phenotypes
+    WHERE phenotype = 'Acute respiratory insufficiency'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM devv5.concept_ancestor ca1
+JOIN devv5.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN devv5.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN devv5.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM dev_covid19.concept_phenotypes
+    WHERE phenotype = 'Acute respiratory insufficiency'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
+
+--reset uncovered concept list
+DELETE FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'not_mapped'
+;
+
+--searching for uncovered concepts in Standard and Source_vocabularies
+INSERT INTO dev_covid19.concept_phenotypes
+SELECT 'Acute respiratory insufficiency',
+       'not_mapped',
+       c.*
+FROM devv5.concept c
+
+WHERE (
+        --To select the specific codes in specific vocabularies
+        --(c.concept_code ~* '^00000|^00000|^00000' AND c.vocabulary_id IN (/*'EDI'*//*, 'KCD7'*/)  ) OR
+
+        --Mask to detect uncovered concepts
+        (c.concept_name ~* '(acute).*(respir).*(fail)|(acute).*(respir).*(insu)'
+
+        --Masks to exclude
+        AND c.concept_name !~* 'History of'
+
+        AND c.domain_id IN ('Condition', 'Observation'/*,'Procedure'*/ /*,'Measurement'*/) --adjust Domains of interest
+
+        AND c.concept_class_id NOT IN ('Substance', 'Organism', 'LOINC Component', 'LOINC System', 'Qualifier Value', 'Answer', 'Survey'/*, 'Morph Abnormality'*/) --exclude useless concept_classes
+
+        AND c.vocabulary_id NOT IN ('MedDRA', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC') --exclude useless vocabularies
+        AND NOT (c.vocabulary_id = 'SNOMED' AND c.invalid_reason IS NOT NULL) --exclude SNOMED invalid concepts
+        AND NOT (c.concept_class_id ~* 'Hierarchy|chapter' AND c.vocabulary_id NOT IN ('EDI', 'KCD7')) --exclude hierarchical concept_classes
+        AND NOT (c.vocabulary_id = 'ICD10CM' AND c.valid_end_date < to_date('20151001', 'YYYYMMDD')) --exclude pre-release ICD10CM codes
+        )
+    )
+    AND NOT EXISTS ( --exclude what is already mapped to Included/Excluded parents (except 'EDI', 'KCD7')
+            SELECT 1
+            FROM devv5.concept_ancestor ca1
+            JOIN devv5.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN devv5.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN devv5.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM dev_covid19.concept_phenotypes
+                WHERE phenotype = 'Acute respiratory insufficiency'
+                    AND criteria IN ('inclusion', 'exclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id NOT IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+    AND NOT EXISTS ( --exclude what is already mapped to Included parents ('EDI', 'KCD7')
+            SELECT 1
+            FROM devv5.concept_ancestor ca1
+            JOIN devv5.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN devv5.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN devv5.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM dev_covid19.concept_phenotypes
+                WHERE phenotype = 'Acute respiratory insufficiency'
+                    AND criteria IN ('inclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+;
+
+--reset Standard concepts Excluded list
+DELETE FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'exclusion'
+;
+
+--NOT NEEDED
+/*
+--List of Standard concepts Excluded
+INSERT INTO dev_covid19.concept_phenotypes
+SELECT 'Acute respiratory insufficiency', 'exclusion', c.*
+FROM devv5.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+
+    )
+;
+
+--List of Standard concepts Excluded for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'exclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Excluded
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM dev_covid19.concept_phenotypes
+WHERE phenotype = 'Acute respiratory insufficiency'
+    AND criteria = 'exclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM devv5.concept_ancestor ca1
+JOIN devv5.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN devv5.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN devv5.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM dev_covid19.concept_phenotypes
+    WHERE phenotype = 'Acute respiratory insufficiency'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+--Concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM devv5.concept_ancestor ca1
+JOIN devv5.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN devv5.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN devv5.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM dev_covid19.concept_phenotypes
+    WHERE phenotype = 'Acute respiratory insufficiency'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
+
+ */
