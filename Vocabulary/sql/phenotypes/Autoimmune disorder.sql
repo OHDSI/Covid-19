e037bb1335e81b1c63f8cb1d69220ba581afe0e6
olegzhuk <oleg.zhuk@odysseusinc.com>
2020-03-19 19:04:13 +0300

    Autoimmune disorder added

---
+++ Vocabulary/sql/phenotypes/Autoimmune disorder.sql
@@ -0,0 +1,421 @@
+--reset phenotype concept list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+;
+
+--reset Standard concepts Included list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'inclusion'
+;
+
+--List of Standard concepts Included
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Autoimmune disorder', 'inclusion', c.*
+FROM @vocabulary_database_schema.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+434621,	--	85828009	Condition	Autoimmune disease	SNOMED
+45765493,	--	702444009	Condition	Autoimmune lymphoproliferative syndrome	SNOMED
+45766036,	--	703119002	Condition	Carditis due to rheumatic fever	SNOMED
+4334806,	--	433228003	Condition	Deformity of foot due to rheumatoid arthritis	SNOMED
+46273442,	--	96531000119109	Condition	Deformity of hand due to rheumatoid arthritis	SNOMED
+4330635,	--	431707004	Condition	Deformity of wrist due to rheumatoid arthritis	SNOMED
+138711,	--	84362007	Condition	Fibrous autoimmune thyroiditis	SNOMED
+4253901,	--	410795001	Condition	Juvenile rheumatoid arthritis	SNOMED
+4199518,	--	432066002	Condition	Lung disorder due to autoimmune disorder	SNOMED
+4051204,	--	123947004	Condition	Post-streptococcal disorder	SNOMED
+4048214,	--	123803006	Condition	Rheumatic arteritis	SNOMED
+37312596,	--	789037005	Condition	Rheumatic cardiomyopathy	SNOMED
+319825,	--	23685000	Condition	Rheumatic heart disease	SNOMED
+4030552,	--	14175009	Condition	Rheumatic joint disease	SNOMED
+4067546,	--	202768008	Condition	Rheumatic torticollis	SNOMED
+4306357,	--	38877003	Condition	Rheumatoid aortitis	SNOMED
+4300202,	--	77522006	Condition	Rheumatoid episcleritis	SNOMED
+4151280,	--	314765000	Condition	Rheumatoid melting disorder of cornea	SNOMED
+4108949,	--	195136004	Condition	Rheumatoid myocarditis	SNOMED
+4299308,	--	402432002	Condition	Rheumatoid neutrophilic dermatitis	SNOMED
+4030424,	--	129563009	Condition	Rheumatoid osteoperiostitis	SNOMED
+4271003,	--	400054000	Condition	Rheumatoid vasculitis	SNOMED
+45768790	--	707297005	Condition	Xerostomia due to autoimmune disease	SNOMED
+
+    )
+;
+
+--List of Standard concepts Included for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'inclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Included
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'inclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Autoimmune disorder'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Autoimmune disorder'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
+
+--reset uncovered concept list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'not_mapped'
+;
+
+--searching for uncovered concepts in Standard and Source_vocabularies
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Autoimmune disorder',
+       'not_mapped',
+       c.*
+FROM @vocabulary_database_schema.concept c
+
+WHERE (
+        --To select the specific codes in specific vocabularies
+        --(c.concept_code ~* '^00000|^00000|^00000' AND c.vocabulary_id IN (/*'EDI'*//*, 'KCD7'*/)  ) OR
+
+        --Mask to detect uncovered concepts
+        (c.concept_name ~* 'Rheum|Autoimmune'
+
+        --Masks to exclude
+        AND c.concept_name !~* 'Monitoring|Non-rheumatic|referral|discharge|poisoning|adverse reaction|modifying|Palindromic|Non-autoimmune|nodule'
+
+        AND c.domain_id IN ('Condition', 'Observation'/*,'Procedure'*/ /*,'Measurement'*/) --adjust Domains of interest
+
+        AND c.concept_class_id NOT IN ('Substance', 'Organism', 'LOINC Component', 'LOINC System', 'Qualifier Value', 'Answer', 'Survey', 'Procedure', 'Observable Entity'/*, 'Morph Abnormality'*/) --exclude useless concept_classes
+
+        AND c.vocabulary_id NOT IN ('MedDRA', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC', 'LOINC') --exclude useless vocabularies
+        AND NOT (c.vocabulary_id = 'SNOMED' AND c.invalid_reason IS NOT NULL) --exclude SNOMED invalid concepts
+        AND NOT (c.concept_class_id ~* 'Hierarchy|chapter' AND c.vocabulary_id NOT IN ('EDI', 'KCD7')) --exclude hierarchical concept_classes
+        AND NOT (c.vocabulary_id = 'ICD10CM' AND c.valid_end_date < to_date('20151001', 'YYYYMMDD')) --exclude pre-release ICD10CM codes
+        )
+    )
+    AND NOT EXISTS ( --exclude what is already mapped to Included/Excluded parents (except 'EDI', 'KCD7')
+            SELECT 1
+            FROM @vocabulary_database_schema.concept_ancestor ca1
+            JOIN @vocabulary_database_schema.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN @vocabulary_database_schema.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN @vocabulary_database_schema.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM @target_database_schema.concept_phenotypes
+                WHERE phenotype = 'Autoimmune disorder'
+                    AND criteria IN ('inclusion', 'exclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id NOT IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+    AND NOT EXISTS ( --exclude what is already mapped to Included parents ('EDI', 'KCD7')
+            SELECT 1
+            FROM @vocabulary_database_schema.concept_ancestor ca1
+            JOIN @vocabulary_database_schema.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN @vocabulary_database_schema.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN @vocabulary_database_schema.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM @target_database_schema.concept_phenotypes
+                WHERE phenotype = 'Autoimmune disorder'
+                    AND criteria IN ('inclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+;
+
+--NOT NEEDED
+/*
+--reset Standard concepts Excluded list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'exclusion'
+;
+
+--List of Standard concepts Excluded
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Autoimmune disorder', 'exclusion', c.*
+FROM @vocabulary_database_schema.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+
+    )
+;
+
+--List of Standard concepts Excluded for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'exclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Excluded
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Autoimmune disorder'
+    AND criteria = 'exclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Autoimmune disorder'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+--Concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Autoimmune disorder'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
+
+ */
