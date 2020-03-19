bcebc9b950e56e6395c27f15f352328851e2b5c3
olegzhuk <oleg.zhuk@odysseusinc.com>
2020-03-19 17:51:31 +0300

    Acute cardiac injury added

---
+++ Vocabulary/sql/phenotypes/Acute cardiac injury.sql
@@ -0,0 +1,417 @@
+--reset phenotype concept list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+;
+
+--reset Standard concepts Included list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'inclusion'
+;
+
+--List of Standard concepts Included
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Acute cardiac injury', 'inclusion', c.*
+FROM @vocabulary_database_schema.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+3048863,	--	49136-5	Measurement	Creatine kinase.MB/Creatine kinase.total [Ratio] in Serum or Plasma	LOINC
+3006044,	--	2158-4	Measurement	Creatine kinase.total/Creatine kinase.MB [Enzymatic activity ratio] in Serum or Plasma	LOINC
+40653873,	--	LG433-9	Measurement	Troponin I.cardiac|MCnc|Pt|ANYBldSerPl	LOINC
+36305612,	--	89577-1	Measurement	Troponin I.cardiac panel - Serum or Plasma by High sensitivity method	LOINC
+37063873,	--	LP385942-0	Measurement	Troponin I.cardiac | Serum or Plasma | Chemistry - non-challenge	LOINC
+36303871,	--	89576-3	Measurement	Troponin T.cardiac panel - Serum or Plasma by High sensitivity method	LOINC
+37045968,	--	LP385946-1	Measurement	Troponin T.cardiac | Serum or Plasma | Chemistry - non-challenge	LOINC
+4253501,	--	74500006	Measurement	Cardiac enzymes/isoenzymes measurement	SNOMED
+4265595,	--	397798009	Measurement	Creatine kinase measurement	SNOMED
+4021291,	--	105000003	Measurement	Troponin measurement	SNOMED
+40761361,	--	58260-1	Observation	Cardiac enzymes	LOINC
+37392188,	--	1000871000000104	Observation	Cardiac enzymes	SNOMED
+37394192,	--	1028101000000105	Observation	Creatine kinase level	SNOMED
+37392625,	--	1006481000000102	Observation	Creatine kinase MB isoenzyme level	SNOMED
+37398480,	--	1001251000000101	Observation	Plasma troponin I level	SNOMED
+37392380,	--	1003361000000108	Observation	Plasma troponin T level	SNOMED
+37392221,	--	1001271000000105	Observation	Serum troponin I level	SNOMED
+37393304 	--	1015261000000102	Observation	Serum troponin T level	SNOMED
+
+    )
+;
+
+--List of Standard concepts Included for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'inclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Included
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'inclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Acute cardiac injury'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Included or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Acute cardiac injury'
+        AND criteria = 'inclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
+
+--reset uncovered concept list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'not_mapped'
+;
+
+--searching for uncovered concepts in Standard and Source_vocabularies
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Acute cardiac injury',
+       'not_mapped',
+       c.*
+FROM @vocabulary_database_schema.concept c
+
+WHERE (
+        --To select the specific codes in specific vocabularies
+        --(c.concept_code ~* '^00000|^00000|^00000' AND c.vocabulary_id IN (/*'EDI'*//*, 'KCD7'*/)  ) OR
+
+        --Mask to detect uncovered concepts
+        (c.concept_name ~* 'Troponin|Cardiac enzymes|ckmb|Creatine kinase'
+
+        --Masks to exclude
+        AND c.concept_name !~* 'qualitative'
+
+        AND c.domain_id IN ('Condition', 'Observation'/*,'Procedure'*/ ,'Measurement') --adjust Domains of interest
+
+        AND c.concept_class_id NOT IN ('Substance', 'Organism', 'LOINC Component', 'LOINC System', 'Qualifier Value', 'Answer', 'Survey'/*, 'Morph Abnormality'*/) --exclude useless concept_classes
+
+        AND c.vocabulary_id NOT IN ('MedDRA', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC') --exclude useless vocabularies
+        AND NOT (c.vocabulary_id = 'SNOMED' AND c.invalid_reason IS NOT NULL) --exclude SNOMED invalid concepts
+        AND NOT (c.concept_class_id ~* 'Hierarchy|chapter' AND c.vocabulary_id NOT IN ('EDI', 'KCD7')) --exclude hierarchical concept_classes
+        AND NOT (c.vocabulary_id = 'ICD10CM' AND c.valid_end_date < to_date('20151001', 'YYYYMMDD')) --exclude pre-release ICD10CM codes
+        )
+    )
+    AND NOT EXISTS ( --exclude what is already mapped to Included/Excluded parents (except 'EDI', 'KCD7')
+            SELECT 1
+            FROM @vocabulary_database_schema.concept_ancestor ca1
+            JOIN @vocabulary_database_schema.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN @vocabulary_database_schema.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN @vocabulary_database_schema.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM @target_database_schema.concept_phenotypes
+                WHERE phenotype = 'Acute cardiac injury'
+                    AND criteria IN ('inclusion', 'exclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id NOT IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+    AND NOT EXISTS ( --exclude what is already mapped to Included parents ('EDI', 'KCD7')
+            SELECT 1
+            FROM @vocabulary_database_schema.concept_ancestor ca1
+            JOIN @vocabulary_database_schema.concept c1
+                ON ca1.descendant_concept_id = c1.concept_id
+            JOIN @vocabulary_database_schema.concept_relationship cr1
+                ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+            JOIN @vocabulary_database_schema.concept c2
+                ON cr1.concept_id_1 = c2.concept_id
+
+            WHERE ca1.ancestor_concept_id IN (
+                SELECT concept_id
+                FROM @target_database_schema.concept_phenotypes
+                WHERE phenotype = 'Acute cardiac injury'
+                    AND criteria IN ('inclusion')
+                    AND concept_id IS NOT NULL
+                    AND criteria IS NOT NULL
+                )
+                AND c2.vocabulary_id IN ('EDI', 'KCD7')
+                AND (c.concept_id = c1.concept_id OR c.concept_id = c2.concept_id)
+        )
+;
+
+--reset Standard concepts Excluded list
+DELETE FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'exclusion'
+;
+
+--List of Standard concepts Excluded
+INSERT INTO @target_database_schema.concept_phenotypes
+SELECT 'Acute cardiac injury', 'exclusion', c.*
+FROM @vocabulary_database_schema.concept c
+WHERE c.concept_id IN (
+--Put concept_ids here
+4041894,	--	166671000	Condition	Cardiac enzymes equivocal	SNOMED
+4042567,	--	166662009	Condition	Cardiac enzymes normal	SNOMED
+40481546,	--	444931001	Condition	High troponin I level	SNOMED
+2212610,	--	84512	Measurement	Troponin, qualitative	CPT4
+3052931,	--	48426-1	Measurement	Troponin T.cardiac [Presence] in Blood	LOINC
+3042837 	--	33204-9	Measurement	Troponin T.cardiac [Presence] in Serum or Plasma	LOINC
+   )
+;
+
+--List of Standard concepts Excluded for comment generation
+SELECT DISTINCT (concept_id || ','), '--', concept_code, domain_id, concept_name, vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'exclusion'
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Markdown-friendly list of Standard concepts Excluded
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || concept_code || '|' || vocabulary_id
+FROM @target_database_schema.concept_phenotypes
+WHERE phenotype = 'Acute cardiac injury'
+    AND criteria = 'exclusion'
+GROUP BY domain_id, concept_id, concept_name, concept_code, vocabulary_id
+ORDER BY domain_id, vocabulary_id, concept_name, concept_code
+;
+
+--Retrieve concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Mapping list
+with mappings as (
+
+SELECT DISTINCT c1.domain_id,
+                c1.concept_id,
+                c1.concept_name,
+                c1.vocabulary_id,
+                c2.vocabulary_id as source_vocabulary_id,
+                string_agg (DISTINCT c2.concept_code, '; ' ORDER BY c2.concept_code) as source_code
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Acute cardiac injury'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+
+GROUP BY    1,2,3,4,5
+)
+
+--list
+/*SELECT domain_id, concept_id, concept_name, vocabulary_id, source_vocabulary_id, source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id*/
+
+--markdown-friendly list
+SELECT domain_id || '|' || concept_id || '|' || concept_name || '|' || vocabulary_id || '|' || source_vocabulary_id || '|' || source_code
+FROM mappings
+ORDER BY domain_id, vocabulary_id, concept_name, concept_id, source_vocabulary_id
+;
+
+--Concepts from source vocabularies mapped to Standard concepts Excluded or any of its child
+--Detailed Mapping list
+with mappings as (
+
+SELECT DISTINCT c2.concept_name as source_code_description,
+                c2.concept_code as source_code,
+                c2.vocabulary_id as source_vocabulary_id,
+                c1.concept_id,
+                c1.concept_code,
+                c1.concept_name,
+                c1.concept_class_id,
+                c1.standard_concept,
+                c1.invalid_reason,
+                c1.domain_id,
+                c1.vocabulary_id
+
+FROM @vocabulary_database_schema.concept_ancestor ca1
+JOIN @vocabulary_database_schema.concept c1
+    ON ca1.descendant_concept_id = c1.concept_id
+JOIN @vocabulary_database_schema.concept_relationship cr1
+    ON ca1.descendant_concept_id = cr1.concept_id_2 AND cr1.relationship_id = 'Maps to' AND cr1.invalid_reason IS NULL
+JOIN @vocabulary_database_schema.concept c2
+    ON cr1.concept_id_1 = c2.concept_id
+WHERE ca1.ancestor_concept_id IN (
+    SELECT concept_id
+    FROM @target_database_schema.concept_phenotypes
+    WHERE phenotype = 'Acute cardiac injury'
+        AND criteria = 'exclusion'
+        AND concept_id IS NOT NULL
+    )
+AND ca1.descendant_concept_id != c2.concept_id
+
+--to add/exclude some vocabularies
+--AND (c2.vocabulary_id like '%ICD%' OR c2.vocabulary_id like '%KCD%')
+AND NOT (c2.vocabulary_id IN ('SNOMED', 'SNOMED Veterinary', 'MeSH', 'CIEL', 'OXMIS', 'DRG', 'SUS', 'Nebraska Lexicon', 'SMQ', 'PPI', 'MDC', 'APC'))
+--AND lower(c1.concept_name) != lower (c2.concept_name)
+)
+
+--list
+/*SELECT *
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id*/
+
+--markdown-friendly list
+SELECT source_code_description || '|' ||
+       source_code || '|' ||
+       source_vocabulary_id || '|' ||
+       concept_id || '|' ||
+       concept_name || '|' ||
+       concept_code || '|' ||
+       concept_class_id || '|' ||
+       --COALESCE (standard_concept, '') || '|' ||
+       --COALESCE (invalid_reason, '') || '|' ||
+       domain_id || '|' ||
+       vocabulary_id
+FROM mappings
+ORDER BY source_code,
+         source_code_description,
+         source_vocabulary_id,
+         concept_id,
+         concept_code,
+         concept_name,
+         concept_class_id,
+         standard_concept,
+         invalid_reason,
+         domain_id,
+         vocabulary_id
+;
